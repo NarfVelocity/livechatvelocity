5bfc0d3fb62a78b342aeb67ac0162036b599028e
NarfVelocity <michel.watteyne@velocityclearingllc.com>
2025-12-26 14:16:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/livechatvelocity/",
-  build: {
-    outDir: "dist",
-    emptyOutDir: true
-  }
+  base: "/livechatvelocity/"
 });
+
